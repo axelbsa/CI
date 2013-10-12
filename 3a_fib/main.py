0a8891f382445520ac8cce1cff8c6a7ab19b86dd
aclk <axelbsa@minicore.(none)>
2013-10-12 16:54:26 +0200

    Updated main.py to explain the complexity

--- 3a_fib/main.py
+++ 3a_fib/main.py
@@ -1,3 +1,20 @@
+########################################################
+# Naive recursive fibonacci implentation.
+# Were n = 4 it will compute the following tree
+#
+#                       fib(4)
+#
+#             fib(3)             fib(2)
+#
+#         fib(2)   fib(1)     fib(1)   fib(0)
+#
+#      fib(1)  fib(0) 
+#
+# This gives roughly a time complexsity of O(2^n) 
+# given that for each call to fib, two new calls 
+# are being done. 
+#######################################################
+
 def fib(n):
     if n == 0:
         return 0
