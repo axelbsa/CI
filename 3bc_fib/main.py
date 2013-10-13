decd140a958a1eb8a42b772d002240892fdb1756
aclk <axelbsa@minicore.(none)>
2013-10-13 15:58:27 +0200

    Changed 3bc to use memoized decorators to cache values between recursion

--- 3bc_fib/main.py
+++ 3bc_fib/main.py
@@ -1,10 +1,32 @@
 import time,sys
+sys.setrecursionlimit(100000)
 
-try: import numpy
-except: print "please install numpy"; sys.exit(-1)
+try: import numpy; f_n = True
+except: f_n = False;print "please install numpy"; sys.exit(-1)
 
-n = 120000
+n = 1200
+f_n = False
 
+def memo(f):
+    cache = {}
+    def inner_memo(n):
+        if n in cache:
+            print cache[n]
+            return cache[n]
+        else:
+            cache[n] = f(n)
+            return cache[n]
+    return inner_memo
+
+@memo
+def fib(n):
+    if n == 0: 
+        return 0
+    elif n == 1:
+        return 1
+    else: 
+        return fib(n-2) + fib(n-1)
+    
 def fib_numpy(n,vector):
     vector[0] = vector[1] = 1
     for index in range(2,n):
@@ -15,9 +37,12 @@ def fib_numpy(n,vector):
 if __name__ == "__main__":
 
     start_tick = time.time()
-    vector = numpy.zeros(n)
-    fib_numpy(n, vector)
-    print vector[len(vector)-1]
+    if f_n:
+        vector = numpy.zeros(n)
+        fib_numpy(n, vector)
+        print vector[len(vector)-1]
+    else:
+        fib(n)
+        
     print "-----------------------------------------------------"
     print "\r\r Total time: %s sec" % (time.time() - start_tick)
-
