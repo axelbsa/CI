b1b11241ff1323528d05a02b661711d6ce4924ba
Axel Sanner <axelsanner@gmail.com>
2015-03-12 16:03:11 +0100

    Update main.py
    added a recursive lambda function

--- pow/main.py
+++ pow/main.py
@@ -1,5 +1,7 @@
 # Inserting random comment, needd"#¤"#¤ a little more comment 
 
+f = lambda n,x: 1 if x==0 else (n*f(n,x-1) if x>0 else 1.0/(n*f(n,abs(x)-1.0)))
+
 def pow(num, power):
     sum_q = 1.0
     if power < 0:
@@ -21,3 +23,5 @@ def rec(n,x):
 
 if __name__ == "__main__":
     print pow(2.0,8.0)
+    print rec(2.0, -3.0)
+    print f(2.0, -3,0)
