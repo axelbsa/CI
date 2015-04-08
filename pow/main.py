5434ffbce46369ca24d76dccdffa3e2cd1afa19c
Axel Sanner <axelsanner@gmail.com>
2015-04-08 10:43:04 +0200

    Renamed pow, to my_pow
    pow is a keyword, don't overwrite it

--- pow/main.py
+++ pow/main.py
@@ -1,8 +1,7 @@
-# Inserting random comment, needd"#¤"#¤ a little more comment 
 
 f = lambda n,x: 1 if x==0 else (n*f(n,x-1) if x>0 else 1.0/(n*f(n,abs(x)-1.0)))
 
-def pow(num, power):
+def my_pow(num, power):
     sum_q = 1.0
     if power < 0:
         sum_q = pow(1.0/num, -1.0 * power)
@@ -22,6 +21,6 @@ def rec(n,x):
         return n*rec(n,x-1)
 
 if __name__ == "__main__":
-    print pow(2.0,8.0)
+    print my_pow(2.0,8.0)
     print rec(2.0, -3.0)
     print f(2.0, -3,0)
